ed6ce3cd01be11e602d2218e531744491805afc6
Italo De la Peña <italoandre.1995@gmail.com>
2021-04-03 14:56:09 -0300

    Amend 'Header' component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header () {
+    return (
+        <div className='header'>
+            <h1>Code challenge!</h1>
+            <h2>Users from <i>Random User API</i></h2>
+        </div>
+    )
+}
