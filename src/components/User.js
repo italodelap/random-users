fd3d9c020432620d49583c0b6212f5ce5c7dae9f
Italo De la Peña <italoandre.1995@gmail.com>
2021-04-03 16:51:44 -0300

    Improve 'User.js'

--- src/components/User.js
+++ src/components/User.js
@@ -3,38 +3,38 @@ import Info from './Info'
 
 export default function User ({ selectedUser }) {
     return (
-        <table className='modal-data' align='center'>
-          <tbody>
-            <tr>
-              <td colSpan='2'>
-                <img 
-                  className='modal-image'
-                  src={selectedUser && selectedUser.picture.large}
-                  alt={selectedUser && selectedUser.name.first}
-                />
-              </td>
-            </tr>
-            <tr>
-              <Info label='Username' data={selectedUser.login.username}></Info>
-              <Info label='Email address' data={selectedUser.email}></Info>
-            </tr>
-            <tr>
-              <Info label='First name' data={selectedUser.name.first}></Info>
-              <Info label='Last name' data={selectedUser.name.last}></Info>
-            </tr>
-            <tr>
-              <Info label='Age' data={selectedUser.dob.age}></Info>
-              <Info label='Cell' data={selectedUser.cell}></Info>
-            </tr>
-            <tr>
-              <Info
-                label='Location'
-                data={selectedUser.location.street.name + ' ' + selectedUser.location.street.number}
-              >
-              </Info>
-              <Info label='Phone' data={selectedUser.phone}></Info>
-            </tr>
-          </tbody>
-        </table>
+      selectedUser &&
+      <table className='modal-data' align='center'>
+        <tbody>
+          <tr>
+            <td colSpan='2'>
+              <img 
+                className='modal-image'
+                src={selectedUser.picture.large}
+                alt={selectedUser.name.first}
+              />
+            </td>
+          </tr>
+          <tr>
+            <Info label='Username' data={selectedUser.login.username}/>
+            <Info label='Email address' data={selectedUser.email}/>
+          </tr>
+          <tr>
+            <Info label='First name' data={selectedUser.name.first}/>
+            <Info label='Last name' data={selectedUser.name.last}/>
+          </tr>
+          <tr>
+            <Info label='Age' data={selectedUser.dob.age}/>
+            <Info label='Cell' data={selectedUser.cell}/>
+          </tr>
+          <tr>
+            <Info
+              label='Location'
+              data={selectedUser.location.street.name + ' ' + selectedUser.location.street.number}
+            />
+            <Info label='Phone' data={selectedUser.phone}/>
+          </tr>
+        </tbody>
+      </table>
     )
 }
