ea11e7794dbb9d37e24281fafc19fc3a37210bb7
Italo De la Peña <italoandre.1995@gmail.com>
2021-04-17 16:11:10 -0300

    Merge the function that removes duplicates with the one that gets users

--- src/components/UsersGrid.js
+++ src/components/UsersGrid.js
@@ -1,74 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
-export default function UsersGrid ({ setSelectedUser, setModalIsOpen }) {
+export default function UsersGrid({ setSelectedUser, setModalIsOpen }) {
+  const [users, setUsers] = useState([])
 
-    const [users, setUsers] = useState([])
+  useEffect(() => {
+    getRandomUsers()
+    // eslint-disable-next-line
+  }, [])
 
-    useEffect(() => {
-        getRandomUsers()
-        // eslint-disable-next-line
-    }, [])
+  // La dependencia podría ser 'users'?
 
-    const removeDuplicateUsers = (duplicateUsers) => {
-        const flag = {}
-        const unique = []
+  const getRandomUsers = async () => {
+    const data = await fetch("https://randomuser.me/api/?results=50")
+    const response = await data.json()
+    const usersData = response.results.filter((user, index) => {
+      return response.results.indexOf(user) === index
+    })
+    
+    setUsers([...users, ...usersData])
+  }
 
-        duplicateUsers.forEach(user => {
-            if (!flag[user.id.value]) {
-                flag[user.id.value] = true
-                unique.push(user)
-            }
-        })
-
-        return unique
-    }
-
-    const getRandomUsers = async () => {
-        const data = await fetch('https://randomuser.me/api/?results=50')
-        const response = await data.json()
-        const usersData = response.results
-        
-        setUsers([...users, ...removeDuplicateUsers(usersData)])
-    }
-
-    return (
-        <InfiniteScroll
-            dataLength={users.length}
-            next={getRandomUsers}
-            hasMore={true}
-            loader={<h4>Loading...</h4>}
-        >
-            <div className='img-grid'>
-                {users &&
-                    users.map((user, index) => (
-                        <div 
-                            className='img-wrap'
-                            key={index + '' + user.id.value}
-                            onClick={() => {
-                                setSelectedUser(user)
-                                setModalIsOpen(true)
-                            }}
-                        >
-                            <img 
-                                alt={user.name.first}
-                                src={user.picture.large}
-                            />
-                            <div className='img-description'>
-                                <p className='img-description-name'>
-                                    <strong>
-                                        {user.name.first} {user.name.last}
-                                    </strong>
-                                </p>
-                                <p className='img-description-location'>
-                                    {user.location.city}, {user.location.state}
-                                </p>
-                            </div>
-                        </div>
-                    ))
-                }
+  return (
+    <InfiniteScroll
+      dataLength={users.length}
+      next={getRandomUsers}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+    >
+      <div className="img-grid">
+        {
+          users && users.map((user, index) => (
+            <div
+              className="img-wrap"
+              key={index + "" + user.id.value}
+              onClick={() => {
+                setSelectedUser(user)
+                setModalIsOpen(true)
+              }}
+            >
+              {/* user-thumbnail */}
+              <img alt={user.name.first} src={user.picture.large} />
+              <div className="img-description">
+                <p className="img-description-name">
+                  <strong>
+                    {user.name.first} {user.name.last}
+                  </strong>
+                </p>
+                <p className="img-description-location">
+                  {user.location.city}, {user.location.state}
+                </p>
+              </div>
             </div>
-        </InfiniteScroll>
-    )
-
+          ))
+        }
+      </div>
+    </InfiniteScroll>
+  )
 }
