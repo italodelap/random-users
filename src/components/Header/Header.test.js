fd8cec4a377fddc534358518aa75b13e90ce0614
Italo De la Peña <italoandre.1995@gmail.com>
2021-04-23 20:40:43 -0300

    Add <Header /> tests

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './index'
+
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+
+describe('<Header />', () => {
+  test('should renders content', () => {
+    const component = render(<Header />)
+    const element = component.getByText('Random User API')
+    expect(element).toBeDefined()
+  })
+})
